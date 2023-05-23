612164a2d4921dddba7669feeee797e9c7f7b704
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2023-05-23 18:36:28 -0300

    Update_Funciones con parametros determinados.py

--- Funciones-15/Funciones con parametros determinados.py
+++ Funciones-15/Funciones con parametros determinados.py
@@ -31,6 +31,50 @@ def imprimirLista(numeros=[]):
 def numeroPar(numero=0):
     return numero % 2 == 0
 
+#sugerencias : 
+funcion1: 
+def subrayar(titulo, subrayado="_"):
+    print(titulo)
+    print(subrayado * len(titulo))
+
+titulo = input("Ingrese título: ")
+subrayar(titulo)
+
+funcion2:
+def areaTriangulo(base=2, altura=2):
+          print("El área del triángulo es de:")
+          resultado = (base * altura) / 2
+    return resultado
+
+print(areaTriangulo(5, 6))
+
+
+funcion3:
+def areaRectangulo(base=2, altura=2):
+    print("El área del rectángulo es de:")
+    return base * altura
+
+area = areaRectangulo(5, 6)
+print(area)
+
+funcion4:
+def imprimirLista(numeros=[]):
+    print("Su lista de números es la siguiente:")
+    for numero in numeros:
+        print(numero)
+
+imprimirLista([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+      
+funcion5:
+def numeroPar(numero=0):
+    return numero % 2 == 0
+
+print(numeroPar(5))
+      
+
+
+
 
 
 
